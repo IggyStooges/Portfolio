1b29708901a4ad2e6cd753109b76f0e4bfead9b3
Дмитрий <selyxaa@yandex.ru>
2019-09-26 17:50:17 +0300

    переопределил пути

--- src/assets/components/portfolio/portfolio.vue
+++ src/assets/components/portfolio/portfolio.vue
@@ -5,25 +5,25 @@
       <li>
         <a href="https://iggystooges.github.io/mishka/build/" class="">
           <span>Портфолио (Стек Vue.JS, SASS, Vue-cli, Vue-router)</span>
-          <img src="dist/portfolio.png" alt="mishka" width="200" height="200">
+          <img src="./img/portfolio.png" alt="mishka" width="200" height="200">
         </a>
       </li>
       <li>
         <a href="https://iggystooges.github.io/veggie/build/" class="">
           <span>Veggie (Стек: HTML, SASS, Gulp, JS)</span>
-          <img src="dist/veggie.png" alt="veggie" width="200" height="200">
+          <img src="./img/veggie.png" alt="veggie" width="200" height="200">
         </a>
       </li>
       <li>
         <a href="https://iggystooges.github.io/mishka/build/" class="">
           <span>Учебный проект "Мишка"(Стек: HTML, SASS, Gulp)</span>
-          <img src="dist/mishka.png" alt="mishka" width="200" height="200">
+          <img src="./img/mishka.png" alt="mishka" width="200" height="200">
         </a>
       </li>
       <li>
         <a href="https://iggystooges.github.io/581613-keksobooking/" class="">
           <span>Учебный проект "Кексобукинг"(Стек: JS (es5))</span>
-          <img src="dist/keksobooking.png" alt="keksobooking" width="200" height="200">
+          <img src="./img//keksobooking.png" alt="keksobooking" width="200" height="200">
         </a>
       </li>
     </ul>
